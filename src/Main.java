397f20374e047ae183261e128f43fabb647124a5
student <sample@bths.edu>
2022-10-27 10:46:19 -0400

    2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter an expression: ");
-        String input = scan.nextLine();
-        Calculator calculator = new Calculator(input);
-        System.out.println("Result: " + calculator.calculate());
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+
+        // write your code here to print the appropriate message(s)
+        // described below
+        if (num<0) {
+            System.out.println("That isn't a positive number.");
+            System.exit(1);
+        }
+        if (num%2==0) {
+            if (num > 1000) {
+                System.out.println("Big Even Number");
+            }
+            else if (num > 100 && num <= 1000) {
+                System.out.println("Medium Even Number");
+            }
+            else {
+                System.out.println("Small Even Number");
+            }
+        }
+        else if (num%2!=0) {
+            if (num%5 ==0) {
+                System.out.println("Divisible by 5");
+            }
+            if (num%7 == 0) {
+                System.out.println("Divisible by 7");
+            }
+        }
+
     }
 }
